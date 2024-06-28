8d943660a6eb899e061c86ce6b6d208b456c81de
Hema74-Spark <hemamadhavi8@gmail.com>
2024-06-28 18:05:37 +0530

    Check for Even AND Positive Number

---
+++ src/main/scala/Even_and_Possitive_1.scala
@@ -0,0 +1,13 @@
+object Even_and_Possitive_1 {
+  def main(args:Array[String]):Unit= {
+    val Number = 14
+    if (Number%2==0 && Number>=0){
+      print("The Number is even and positive")
+    }
+    else
+      {
+        print("The Number is not even and positive")
+      }
+
+  }
+}
